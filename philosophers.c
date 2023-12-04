abb3ba4811e473915a42fb0b83d1fd13458f0695
Oreste <tuoreste@gmail.com>
2023-12-04 01:24:59 +0100

    mutex_initiated

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: otuyishi <otuyishi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:30:10 by otuyishi          #+#    #+#             */
-/*   Updated: 2023/12/03 17:13:52 by otuyishi         ###   ########.fr       */
+/*   Updated: 2023/12/04 01:11:17 by otuyishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,16 +51,41 @@ void	clean_args(int argc, char **argv)
 		error_exit("Wrong no of inputs");
 }
 
+int	mutex_initiated(t_philo *phil, t_data *data)
+{
+	pthread_mutex_t	mutex_philo;
+
+	phil->forks = (t_philo *)malloc(sizeof(t_philo) * phil->num_of_philos);
+	if (!phil->forks)
+		return (1);
+	if (pthread_mutex_init(&mutex_philo, phil->forks))
+		return (1);
+	if (pthread_mutex_init(&mutex_philo, phil->print))
+		return (1);
+	if (pthread_mutex_init(&mutex_philo, phil->eating))
+		return (1);
+	if (pthread_mutex_init(&mutex_philo, phil->done_eating))
+		return (1);
+	if (pthread_mutex_init(&mutex_philo, phil->forks))
+		return (1);
+}
+
 int	put_data(t_philo *phil, t_data *data, char **argv)
 {
-	t_data->num_of_philos = ft_atoi(argv[1]);
-	t_data.death_clock = argv[2];
-	t_data.eat_clock = argv[3];
-	t_data.sleep_clock = argv[4];
-	t_philo.time = 0;
-	t_philo.
+	phil->num_of_philos = ft_atoi(argv[1]);
+	data->death_clock = ft_atoi(argv[2]);
+	data->eat_clock = ft_atoi(argv[3]);
+	data->sleep_clock = ft_atoi(argv[4]);
+	phil->time = 0;
+	phil->done_eating = 0;
+	phil->is_dead = 0;
 	if (argv[5])
-		t_data.times_to_eat = argv[5];
+		data->times_to_eat = ft_atoi(argv[5]);
+	if (phil->num_of_philos < 1 || phil->num_of_philos > 200 || \
+		data->death_clock < 0 || data->eat_clock < 0 || data->sleep_clock < 0)
+		error_exit("Try again! input is invalid");
+	if (mutex_initiated(&phil, &data) || philosophers_initiated(&phil, &data))
+		error_exit("Error initilizing (mutex/threads of philos)");
 }
 
 int	lets_go(char **argv)
@@ -75,7 +100,7 @@ int	lets_go(char **argv)
 	if (!data)
 		return (free(phil), 1);
 	put_data(&phil, &data, argv);
-	free_up();
+	//free_up() t_phil and t_data;
 }
 
 int	main(int argc, char **argv)
--- philosophers.h
+++ philosophers.h
@@ -6,28 +6,34 @@
 /*   By: otuyishi <otuyishi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:27:26 by otuyishi          #+#    #+#             */
-/*   Updated: 2023/12/03 16:45:34 by otuyishi         ###   ########.fr       */
+/*   Updated: 2023/12/04 01:03:38 by otuyishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
-# include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
 
 typedef struct s_philo
 {
 	pthread_t	philosophe;
+	int			num_of_philos;
 	int			time;
+	int			forks;
+	int			eating;
+	int			done_eating;
+	int			is_dead;
+	int			lst_time_eating;
+	int			print;
 
 }		t_philo;
 
 typedef struct s_data
 {
 	t_philo		*philo;
-	int			num_of_philos;
 	int			death_clock;
 	int			eat_clock;
 	int			sleep_clock;
